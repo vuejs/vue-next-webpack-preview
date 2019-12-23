986c3d93af5324181f859f2ed3a92b51113b3dbe
Evan You <yyx990803@gmail.com>
2019-12-23 11:36:46 -0500

    add alias

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = (env = {}) => ({
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/'
   },
+  resolve: {
+    alias: {
+      // this isn't technically needed, since the default `vue` entry for bundlers
+      // is a simple `export * from '@vue/runtime-dom`. However having this
+      // extra re-export somehow causes webpack to always invalidate the module
+      // on the first HMR update and causes the page to reload.
+      'vue': '@vue/runtime-dom'
+    }
+  },
   module: {
     rules: [
       {
